15ffb6ca37f9771e9c19545d10400a3bcde0a8e3
HironoriSato <maza.hs5360@gmail.com>
2020-08-29 23:41:32 +0900

    [add]ProductList compを作成

---
+++ src/features/product/ProductList.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { Container, Row, Col, Card } from 'react-bootstrap'
+
+const ProductList = ({ data: { products } }) => {
+  return (
+    <Container>
+      <Row>
+        {products.map((product, i) => {
+          return (
+            <Col as={Link} to="/" sm={2} lg={3} className="mt-3" key={Number(product.productId)}>
+              <Card>
+                <Card.Img variant="top" src={product.imgUrl} />
+                <Card.Body>
+                  <Card.Subtitle className="text-dark font-weight-bold">{product.productNm}</Card.Subtitle>
+                  <Card.Text className="d-inline-block text-dark font-weight-bold">{`¥${product.productPrice}`}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          )
+        })}
+      </Row>
+    </Container>
+  )
+}
+
+export default ProductList
--- src/features/product/ProductListPage.jsx
+++ src/features/product/ProductListPage.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  setUserId,
+  setProducts,
+  fetchProducts
+} from './productSlice'
 
+import { Container, Row, Col } from 'react-bootstrap'
 import Title from './Title'
+import ProductList from './ProductList'
+import FilterSortNav from '../../components/filterSortNav/FilterSortNav'
+import PaginationBar from '../../components/paginationBar/PaginationBar'
 
 const ProductListPage = () => {
+  const dispatch = useDispatch()
+  const { userId, products } = useSelector(state => state.product)
+  const [pageNum, setPageNum] = useState(1) // 商品一覧の現在のページ数
+  const limitNum = 8 // 商品一覧に表示する商品数
+  const offset = limitNum * (pageNum - 1)
+  const filteredProducts = products.filter((product, i) => (offset <= i && i <= (limitNum-1) * pageNum))
+
+  useEffect(() => {
+    dispatch(fetchProducts(0))
+  }, [])
 
   return (
     <>
       <Title data={{ title: "商品" }} />
+      <ProductList data={{
+        products: filteredProducts
+      }} />
     </>
   )
 }
